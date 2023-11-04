490ec5b741bdadd1295ef5cf655557473f7b324e
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-11-04 23:27:26 +0100

    Add files via upload

---
+++ monthKalendarKontrola/monthKalendarKontrola/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace monthKalendarKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(164, 53);
+            this.monthCalendar1.MaxSelectionCount = 999;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(135, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Odaberite termin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 387);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Upotreba MonthKalendar Kontrola";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ monthKalendarKontrola/monthKalendarKontrola/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monthKalendarKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan dani = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart;
+
+            if (dani.Days < 1)
+            {
+                     MessageBox.Show("Tipkom SHIFT odaberite željeni datum sa brojem dana većim od 0.");
+                   return;            
+            }
+
+            MessageBox.Show("Vaša rezervacija traje: " + dani.Days.ToString() + " dana.");
+
+            MessageBox.Show("Vaša rezervacija počinje " + monthCalendar1.SelectionStart.ToShortDateString() + " i traje vam do " + monthCalendar1.SelectionEnd.ToShortDateString());
+        }
+    }
+}
